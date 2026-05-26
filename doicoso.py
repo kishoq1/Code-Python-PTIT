4172f9f151472b5b826eef36fd8862eab88bf330
QuanPVT <anquan0298@gmail.com>
2026-05-26 23:04:54 +0700

    Create doicoso.py

---
+++ doicoso.py
@@ -0,0 +1,21 @@
+str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+str = list(str)
+
+t = int(input())
+for _ in range(t):
+    s = input().split()
+    a, b = int(s[0]), int(s[1])
+    arr = []
+    while a > 0:
+        k = a % b
+        if k < 10:  arr.append(k)
+        else:
+            for i in str:
+                if k == ord(i) - 55:
+                    arr.append(i)
+                    break
+        a = int(a/b)
+    arr.reverse()
+    for i in arr:
+        print(i, end='')
+    print('')
