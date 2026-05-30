776d7fd4d2fb856a9370c9976b505821ef39fc62
QuanPVT <anquan0298@gmail.com>
2026-05-30 12:10:36 +0700

    Create sapdatlaixaukytu.py

---
+++ sapdatlaixaukytu.py
@@ -0,0 +1,9 @@
+t = int(input())
+for i in range(t):
+    s1 = list(input())
+    s2 = list(input())
+    s1.sort()
+    s2.sort()
+    print("Test", (i+1), end=': ')
+    if s1 == s2: print("YES")
+    else: print("NO")
