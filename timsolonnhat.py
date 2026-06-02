6ffed072dfd24a2ed703deaf1421081eff60de18
QuanPVT <anquan0298@gmail.com>
2026-06-02 10:14:53 +0700

    Create timsolonnhat.py

---
+++ timsolonnhat.py
@@ -0,0 +1,13 @@
+import re
+t = int(input()) 
+while t > 0:
+    s = input()
+    #m1 = re.findall(r'\d', s) tach tung chu so
+    s1 = re.findall(r'\d+', s) #tach tung so
+    a = []
+    for x in s1:
+        a.append(int(x))
+
+    print(max(a))
+  
+    t -= 1
